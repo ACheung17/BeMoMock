b3883e379bf5005e8ebb3cdf39b83048764c98e3
Ada Cheung <ada.cheung96@gmail.com>
2020-07-29 10:25:31 -0400

    Updates

---
+++ precache-manifest.d515eb6b3d97fa45a9ca1285e538c397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250bb47b403d52288eff055ea1dfa560",
+    "url": "/BeMoMock/index.html"
+  },
+  {
+    "revision": "91b7a136d657b8d95f37",
+    "url": "/BeMoMock/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "91b7a136d657b8d95f37",
+    "url": "/BeMoMock/static/js/2.519f4a90.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BeMoMock/static/js/2.519f4a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125ab69ddb3ab84922e8",
+    "url": "/BeMoMock/static/js/main.641ec10b.chunk.js"
+  },
+  {
+    "revision": "1984744c54271524865a",
+    "url": "/BeMoMock/static/js/runtime-main.e7ec8606.js"
+  }
+]);
